98a57a633ffffd487a30ca521bc50bd9c37b437c
T-Pirozzini <tpirozzini@gmail.com>
2023-10-13 14:45:02 -0700

    load in and animate sprites

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:shift_wizard_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ShiftWizardApp());
+    // await tester.pumpWidget(ShiftWizardApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
